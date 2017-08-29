30459bfae87c2ea19ece6907becc7b54bd6372e2
swapsCAPS <verstegen.daan@gmail.com>
2017-08-29 19:56:56 +0200

    force install script

---
+++ force-install.sh
@@ -0,0 +1,5 @@
+ln -sf ~/src/teh-awesome-vim-setup/.vim ~/.vim
+ln -sf ~/src/teh-awesome-vim-setup/.vimrc ~/.vimrc
+ln -sf ~/src/teh-awesome-vim-setup/.vim ~/.config/nvim
+ln -sf ~/.vimrc ~/.config/nvim/init.vim
+
