dcb9c0c16f0919559c183801337524995a7088af
swapsCAPS <verstegen.daan@gmail.com>
2024-05-20 22:50:27 +0200

    Update shortcuts

--- nvim/lua/setup/hotkeys.lua
+++ nvim/lua/setup/hotkeys.lua
@@ -1,18 +1,25 @@
 local nvimTree = require('nvim-tree.api')
 local telescope = require('telescope.builtin')
+local neogit = require('neogit')
 
-vim.keymap.set('n', '<F4>', nvimTree.tree.toggle, { noremap = true })
-vim.keymap.set('n', '<F5>', ':w <Enter>', { noremap = true })
-vim.keymap.set('n', '<F6>', ':set hlsearch!<Enter>', { noremap = true })
-vim.keymap.set('n', '<F10>', ':bd <Enter>', { noremap = true })
-vim.keymap.set('n', '<leader>ki', vim.diagnostic.open_float, { noremap = true, desc = "Open float window (using <leader>ki since vscode uses cmd-k_cmd-i binding)" })
-vim.keymap.set('v', '<leader>=', ':EasyAlign=<Enter>', { noremap = true })
-vim.keymap.set('v', '<leader>:', ':EasyAlign:<Enter>', { noremap = true })
-vim.keymap.set('v', '<leader>,', ':EasyAlign*,<Enter>', { noremap = true })
+vim.keymap.set('n', '<F2>', vim.lsp.buf.rename, {})
+vim.keymap.set('n', '<F3>', neogit.open, {})
+vim.keymap.set('n', '<F4>', nvimTree.tree.toggle, {})
+vim.keymap.set('n', '<F5>', ':w <Enter>', {})
+vim.keymap.set('n', '<F6>', ':set hlsearch!<Enter>', {})
+vim.keymap.set('n', '<F10>', ':bd <Enter>', {})
+vim.keymap.set('v', '<leader>=', ':EasyAlign=<Enter>', {})
+vim.keymap.set('v', '<leader>:', ':EasyAlign:<Enter>', {})
+vim.keymap.set('v', '<leader>,', ':EasyAlign*,<Enter>', {})
 
-vim.keymap.set('i', 'jj', '<Esc>', { noremap = true })
+vim.keymap.set('i', 'jj', '<Esc>', {})
 
-vim.keymap.set('n', '<leader>fo', vim.lsp.buf.format, { noremap = true })
+vim.keymap.set('n', '<leader>of', vim.diagnostic.open_float, {
+  desc = [[Open float window (using <leader>ki since vscode uses cmd-k_cmd-i binding)]]
+})
+vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
+
+vim.keymap.set('n', '<leader>fo', vim.lsp.buf.format, {})
 
 vim.keymap.set('n', '<leader>ff', telescope.find_files, {})
 vim.keymap.set('n', '<leader>rg', telescope.live_grep, {})
