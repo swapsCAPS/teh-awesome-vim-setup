07e5db2793be6ca43958f49ddfa482ce166b08aa
swapsCAPS <verstegen.daan@gmail.com>
2024-05-20 22:58:49 +0200

    Organize TS imports with <leader>oi

--- nvim/lua/setup/hotkeys.lua
+++ nvim/lua/setup/hotkeys.lua
@@ -2,6 +2,13 @@ local nvimTree = require('nvim-tree.api')
 local telescope = require('telescope.builtin')
 local neogit = require('neogit')
 
+local function organizeTsImports()
+  vim.lsp.buf.execute_command({
+    command = "_typescript.organizeImports",
+    arguments = { vim.fn.expand("%:p") }
+  })
+end
+
 vim.keymap.set('n', '<F2>', vim.lsp.buf.rename, {})
 vim.keymap.set('n', '<F3>', neogit.open, {})
 vim.keymap.set('n', '<F4>', nvimTree.tree.toggle, {})
@@ -20,6 +27,10 @@ vim.keymap.set('n', '<leader>of', vim.diagnostic.open_float, {
 vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
 
 vim.keymap.set('n', '<leader>fo', vim.lsp.buf.format, {})
+
+vim.keymap.set('n', '<leader>oi', organizeTsImports, {
+  desc = "Organize TS imports"
+})
 
 vim.keymap.set('n', '<leader>ff', telescope.find_files, {})
 vim.keymap.set('n', '<leader>rg', telescope.live_grep, {})
