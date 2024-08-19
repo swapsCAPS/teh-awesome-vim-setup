641f33387a3053398659f5f74cd28a324ac7fefd
swapsCAPS <verstegen.daan@gmail.com>
2024-08-19 08:16:41 +0200

    run linters after save

--- nvim/lua/setup/nvim-lint.lua
+++ nvim/lua/setup/nvim-lint.lua
@@ -5,3 +5,11 @@ lint.linters_by_ft = {
   yaml = { "yamllint" },
   ghaction = { "actionlint" }
 }
+
+vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+  callback = function()
+    -- try_lint without arguments runs the linters defined in `linters_by_ft`
+    -- for the current filetype
+    require("lint").try_lint()
+  end,
+})
