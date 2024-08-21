0a179a68fd6522ff51115d22bfcd677d386cbe5d
swapsCAPS <verstegen.daan@gmail.com>
2024-08-21 09:30:15 +0200

    Update hotkeys

--- nvim/lua/setup/hotkeys.lua
+++ nvim/lua/setup/hotkeys.lua
@@ -3,27 +3,26 @@ local telescope = require("telescope.builtin")
 local neogit = require("neogit")
 local dapui = require("dapui")
 
-vim.keymap.set("n", "<F2>", vim.lsp.buf.rename, {})
-vim.keymap.set("n", "<F3>", neogit.open, {})
-vim.keymap.set("n", "<F4>", nvimTree.tree.toggle, {})
-vim.keymap.set("n", "<F5>", ":w <Enter>", {})
-vim.keymap.set("n", "<F6>", ":set hlsearch!<Enter>", {})
-vim.keymap.set("n", "<F10>", ":bd <Enter>", {})
-vim.keymap.set("v", "<leader>=", ":EasyAlign=<Enter>", {})
-vim.keymap.set("v", "<leader>:", ":EasyAlign:<Enter>", {})
-vim.keymap.set("v", "<leader>,", ":EasyAlign*,<Enter>", {})
+vim.keymap.set("n", "<F2>", vim.lsp.buf.rename, { desc = "Rename symbol using LSP" })
+vim.keymap.set("n", "<F4>", nvimTree.tree.toggle, { desc = "Toggle file tree" })
+vim.keymap.set("n", "<F5>", ":w <Enter>", { desc = "Jesus quick saves" })
+vim.keymap.set("n", "<F6>", ":set hlsearch!<Enter>", { desc = "Toggle search highlighting" })
+vim.keymap.set("n", "<F10>", ":bd <Enter>", { desc = "Delete buffer" })
+vim.keymap.set("v", "<leader>=", ":EasyAlign=<Enter>", { desc = "EasyAlign =" })
+vim.keymap.set("v", "<leader>:", ":EasyAlign:<Enter>", { desc = "EasyAlign :" })
+vim.keymap.set("v", "<leader>,", ":EasyAlign*,<Enter>", { desc = "EasyAlign *" })
 
 vim.keymap.set("i", "jj", "<Esc>", {})
 
-vim.keymap.set("n", "<leader>d", dapui.float_element, {})
-vim.keymap.set("n", "<leader>D", dapui.toggle, {})
+vim.keymap.set("n", "<leader>d", dapui.float_element, { desc = "DAP UI float element" })
+vim.keymap.set("n", "<leader>D", dapui.toggle, { desc = "Toggle DAP UI" })
 
-vim.keymap.set("n", "<leader>of", vim.diagnostic.open_float, {})
-vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+vim.keymap.set("n", "<leader>of", vim.diagnostic.open_float, { desc = "vim.diagnostic.open_float" })
+vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "vim.lsp.buf.code_action" })
 
-vim.keymap.set("n", "<leader>ng", neogit.open, {})
+vim.keymap.set("n", "<leader>ng", neogit.open, { desc = "Open NeoGit" })
 
-vim.keymap.set("n", "<leader>fo", vim.lsp.buf.format, {})
+vim.keymap.set("n", "<leader>fo", vim.lsp.buf.format, { desc = "Format buffer using LSP" })
 
 vim.keymap.set("n", "<leader>oi", function()
   vim.lsp.buf.execute_command({
@@ -36,14 +35,13 @@ vim.keymap.set("n", "<C-P>", telescope.find_files, {})
 vim.keymap.set("n", "<C-F>", function()
   telescope.grep_string({ search = "" })
 end, { desc = "Fuzzy find" })
-vim.keymap.set("n", "<C-B>", telescope.buffers, {})
-vim.keymap.set("n", "<leader>fh", telescope.help_tags, {})
+vim.keymap.set("n", "<C-B>", telescope.buffers, { desc = "Telescope open buffers" })
+vim.keymap.set("n", "<leader>fh", telescope.help_tags, { desc = "Telescope help tags" })
 
-vim.keymap.set("n", "]g", vim.diagnostic.goto_next, {})
-vim.keymap.set("n", "[g", vim.diagnostic.goto_prev, {})
-
-vim.api.nvim_set_keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "LSP go to definition", noremap = true, silent = true })
+vim.keymap.set("n", "gu", telescope.lsp_references,
+  { desc = "Telescope LSP references (usages)", noremap = true, silent = true })
 
 vim.keymap.set("n", "[c", function()
   require("treesitter-context").go_to_context(vim.v.count1)
-end, { silent = true })
+end, { desc = "Go to parent", silent = true })
