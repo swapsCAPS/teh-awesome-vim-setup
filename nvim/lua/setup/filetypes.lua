84813ec1572cf918d57724a41c2ca1affb5d6492
swapsCAPS <verstegen.daan@gmail.com>
2024-08-21 09:29:06 +0200

    Add docker compose filetypes
    Isn't there a plugin that does this for a lot of filetypes in one go?

---
+++ nvim/lua/setup/filetypes.lua
@@ -0,0 +1,8 @@
+vim.filetype.add({
+  filename = {
+    ["docker-compose.yml"] = "yaml.docker-compose",
+    ["docker-compose.yaml"] = "yaml.docker-compose",
+    ["compose.yml"] = "yaml.docker-compose",
+    ["compose.yaml"] = "yaml.docker-compose",
+  },
+})
