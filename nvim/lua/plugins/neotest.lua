c4bc364fab3f7d756d19c687b5900cd584f03bb7
swapsCAPS <verstegen.daan@gmail.com>
2024-10-10 21:34:53 +0200

    trouble + neotest stuffs

--- nvim/lua/plugins/neotest.lua
+++ nvim/lua/plugins/neotest.lua
@@ -1,21 +1,84 @@
 return {
-  {
-    "rcasia/neotest-java",
-    lazy = true,
+  "nvim-neotest/neotest",
+  dependencies = {
+    "nvim-neotest/nvim-nio",
+    "nvim-lua/plenary.nvim",
+    "nvim-treesitter/nvim-treesitter",
+    "antoinemadec/FixCursorHold.nvim",
+    "nvim-neotest/neotest-jest",
+    "adrigzr/neotest-mocha",
   },
-  {
-    "nvim-neotest/neotest",
-    dependencies = {
-      "nvim-neotest/nvim-nio",
-      "nvim-lua/plenary.nvim",
-      "antoinemadec/FixCursorHold.nvim",
-      "nvim-treesitter/nvim-treesitter",
-    },
-    opts = {
+  opts = function()
+    return {
       adapters = {
-        ["neotest-java"] = {},
+        require("neotest-jest"),
+        require("neotest-mocha")({
+          command = "npm test --",
+          command_args = function(context)
+            return {
+              "--full-trace",
+              "--reporter=json",
+              "--reporter-options=output=" .. context.results_path,
+              "--grep=" .. context.test_name_pattern,
+            }
+          end,
+          env = { CI = true },
+          cwd = function(path)
+            return vim.fn.getcwd()
+          end,
+        }),
       },
+    }
+  end,
+  keys = {
+    {
+      "<leader>tt",
+      function()
+        require("neotest").run.run(vim.fn.expand("%"))
+      end,
+      desc = "Run File",
     },
-    lazy = true,
+    {
+      "<leader>tT",
+      function()
+        require("neotest").run.run(vim.loop.cwd())
+      end,
+      desc = "Run All Test Files",
+    },
+    {
+      "<leader>tr",
+      function()
+        require("neotest").run.run()
+      end,
+      desc = "Run Nearest",
+    },
+    {
+      "<leader>ts",
+      function()
+        require("neotest").summary.toggle()
+      end,
+      desc = "Toggle Summary",
+    },
+    {
+      "<leader>to",
+      function()
+        require("neotest").output.open({ enter = false, auto_close = true })
+      end,
+      desc = "Show Output",
+    },
+    {
+      "<leader>tO",
+      function()
+        require("neotest").output_panel.toggle()
+      end,
+      desc = "Toggle Output Panel",
+    },
+    {
+      "<leader>tS",
+      function()
+        require("neotest").run.stop()
+      end,
+      desc = "Stop",
+    },
   },
 }
--- nvim/lua/plugins/nvim-dap-ui.lua
+++ nvim/lua/plugins/nvim-dap-ui.lua
@@ -1,6 +1,5 @@
 return {
   "rcarriga/nvim-dap-ui",
-  lazy = true,
   dependencies = {
     "mfussenegger/nvim-dap",
     "nvim-neotest/nvim-nio",
