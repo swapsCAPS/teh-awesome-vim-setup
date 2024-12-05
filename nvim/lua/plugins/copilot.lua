dc9b99850849b43eb87d6168f7ee369bf5767fa8
swapsCAPS <verstegen.daan@gmail.com>
2024-12-05 09:10:30 +0100

    copilot meh

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -1,1 +1,14 @@
-return { enabled = false, "github/copilot.vim" }
+return {
+  "github/copilot.vim",
+  enabled = false,
+  event = "BufEnter",
+  config = function()
+    vim.keymap.set("i", "<C-J>", 'copilot#Accept("\\<CR>")', {
+      expr = true,
+      replace_keycodes = false,
+    })
+    vim.g.copilot_no_tab_map = true
+
+    return {}
+  end,
+}
